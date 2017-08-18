c788a52eb588e16d30a96abe1032fe223f4263b3
Maksim42 <maksim.lomako@gmail.com>
2017-08-18 20:36:24 +0300

    TextBox: add special symbol procesing

--- TextBox.cs
+++ TextBox.cs
@@ -22,13 +22,20 @@ namespace SimpleConsole
 
         public static TextBox GetTextBox(int width, int maxHeight, string text)
         {
-            int neededHeight = text.Length / width +
-                ((text.Length % width > 0) ? 1 : 0);
+            int neededHeight = 0;
+            var tempBox = new TextBox(width, 1);
+            tempBox.Text = text;
 
-            if (neededHeight > maxHeight)
+            foreach (var line in tempBox.Lines())
             {
-                neededHeight = maxHeight;
-            } 
+                if (neededHeight >= maxHeight)
+                {
+                    neededHeight = maxHeight;
+                    break;
+                }
+
+                neededHeight++;
+            }
 
             TextBox resultBox = new TextBox(width, neededHeight);
             resultBox.Text = text;
@@ -93,24 +100,52 @@ namespace SimpleConsole
 
         private IEnumerable<string> Lines()
         {
-            int curentLine = 0;
-            int lineLength = width;
+            int lineStart = 0;
+            int lineLength;
             bool nextLine = true;
 
             while (nextLine)
             {
-                if (curentLine + lineLength > text.Length)
+                lineLength = width;
+
+                if (lineStart + lineLength > text.Length)
                 {
-                    lineLength = text.Length - curentLine;
-                    nextLine = false;
+                    lineLength = text.Length - lineStart;
                 }
 
-                yield return text.ToString(curentLine, lineLength);
+                string resultLine = text.ToString(lineStart, lineLength);
 
-                curentLine += width;
+                lineLength = LineProcesing(ref resultLine);
+
+                yield return resultLine;
+
+                lineStart += lineLength;
+
+                if (lineStart == text.Length)
+                {
+                    nextLine = false;
+                }
             }
 
             yield break;
+        }
+
+        private int LineProcesing(ref string resultLine)
+        {
+            int resultShift = resultLine.Length;
+            string newLine = resultLine;
+
+            int findIndex = newLine.IndexOf('\n');
+            if (findIndex != -1)
+            {
+                newLine = newLine.Substring(0, findIndex);
+                resultShift = findIndex + 1;
+            }
+
+            newLine = newLine.Replace('\t', ' ');
+
+            resultLine = newLine;
+            return resultShift;
         }
     }
 }
